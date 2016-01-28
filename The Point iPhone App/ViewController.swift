95d07359c69d8fee7600272c3c339f2aca0a16e0
rOmAn2193 <roman_puzey@aol.com>
2016-01-28 11:11:06 -0700

    Initial Commit

---
+++ The Point iPhone App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Point iPhone App
+//
+//  Created by Roman on 1/28/16.
+//  Copyright © 2016 Roman Puzey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
