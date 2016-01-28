7ea3027da44bc3a4eedd727c4774112073ecfaa9
rOmAn2193 <roman_puzey@aol.com>
2016-01-28 12:48:05 -0700

    first commit

--- The Point iPhone App/MainVC.swift
+++ The Point iPhone App/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  The Point iPhone App
 //
 //  Created by Roman on 1/28/16.
@@ -8,18 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func loadMenu(sender: AnyObject)
+    {
+        performSegueWithIdentifier("menu", sender: nil)
+    }
 
 }
-
